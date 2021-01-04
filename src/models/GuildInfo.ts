209576f897db8896e50cd77e217da8aa176f5d83
Sycrixx <biometricmc@gmail.com>
2021-01-05 06:37:34 +1100

    Create GuildInfo schema

---
+++ src/models/GuildInfo.ts
@@ -0,0 +1,56 @@
+import { model, Schema, Document } from "mongoose";
+
+export interface IGuildInfo extends Document {
+    guildName: string,
+    memberCount: string,
+    region: string,
+    // categories: string,
+    // textChannels: string,
+    // voiceChannels: string,
+    // roles: string,
+    prefix: string,
+}
+
+const GuildInfoSchema = new Schema({
+    guildName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    memberCount: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    region: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    categories: {
+        type: Array,
+        required: true,
+        unique: true,
+    },
+    textChannels: {
+        type: Array,
+        required: true,
+        unique: true,
+    },
+    voiceChannels: {
+        type: Array,
+        required: true,
+        unique: true,
+    },
+    roles: {
+        type: Array,
+        required: true,
+        unique: true,
+    },
+    prefix: {
+        type: String,
+        default: "!",
+    }
+});
+
+export default model<IGuildInfo>("GuildInfo", GuildInfoSchema);
