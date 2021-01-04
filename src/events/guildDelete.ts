78ae8dda6e98757755c9b4f91458664fc0e88c13
Sycrixx <biometricmc@gmail.com>
2021-01-05 07:00:44 +1100

    Add deleting guild data on leave

---
+++ src/events/guildDelete.ts
@@ -0,0 +1,13 @@
+import { SynicClient } from "../struct/Client";
+import { Guild } from "discord.js";
+
+// Import GuildInfo Model
+import GuildInfo from "../models/GuildInfo";
+
+module.exports = async (client:SynicClient, guildData:Guild) => {
+    const data = GuildInfo.findOne({ guildId: guildData.id }, (err:any) => {
+        if (err) return console.error(err);
+    });
+
+    data.remove();
+}
