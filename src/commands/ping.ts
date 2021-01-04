b45a71d0a6009fa4d23f4d73ea4390945cee8309
Sycrixx <biometricmc@gmail.com>
2021-01-05 05:11:45 +1100

    Create ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,16 @@
+import { SynicClient } from "../struct/Client";
+import { Message } from "discord.js";
+
+export default {
+    name: "ping",
+    description: "Get bot & api latency",
+    category: ["utility"],
+    /**
+     * @param {SynicClient} client
+     * @param {Message} message
+     */
+    async execute(client:SynicClient, message:Message) {
+        const msg = await message.channel.send("Pinging... 🏓");
+        await msg.edit(`**Pong!** 🏓\nBot Latency: \`${msg.createdTimestamp - message.createdTimestamp}ms\` \nGateway Latency: \`${client.ws.ping}ms\``);
+    }
+}
