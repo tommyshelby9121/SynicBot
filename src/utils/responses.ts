bf81cf8681854788c36833592e877332df97f111
Sycrixx <biometricmc@gmail.com>
2021-01-09 10:17:22 +1100

    Add responses for moderation (warn)

---
+++ src/utils/responses.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  en: {
+    moderation: {
+      no_user_permission: "You do not have permission to execute this command!",
+      no_bot_permission: "I do not have permission to execute this command!",
+      no_user_provided: "Please provide a user to ",
+      warn: {
+        could_not_warn_user: "Could not warn user! Please remove all permissions from the user in order to warn them!",
+        cannot_warn_self: "You cannot warn yourself!"
+      }
+    }
+  }
+}
