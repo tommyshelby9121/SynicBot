5d78968984de49e1044d2138d75f2656da159e99
Sycrixx <biometricmc@gmail.com>
2021-01-09 10:05:33 +1100

    Create noPermission embed

---
+++ src/utils/embedBuilder.ts
@@ -0,0 +1,10 @@
+import { Message, MessageEmbed } from "discord.js";
+
+export async function noPermission(message:Message, description:any) {
+    const embed = new MessageEmbed()
+        .setDescription(description)
+        .setColor("#E74C3C")
+        .setFooter(`Requested By: ${message.author.tag} | Synic v1.0.0`);
+
+    await message.channel.send(embed);
+}
