be250ed35219e44070b0aee0c974c3ddec49aaf2
Sycrixx <biometricmc@gmail.com>
2021-01-09 10:17:03 +1100

    Create noUser & warnCancelled embed

--- src/utils/embedBuilder.ts
+++ src/utils/embedBuilder.ts
@@ -8,3 +8,21 @@ export async function noPermission(message:Message, description:any) {
 
     await message.channel.send(embed);
 }
+
+export async function noUser(message:Message, description:any) {
+    const embed = new MessageEmbed()
+        .setDescription(description)
+        .setColor("#E74C3C")
+        .setFooter(`Requested By: ${message.author.tag} | Synic v1.0.0`);
+
+    await message.channel.send(embed);
+}
+
+export async function warnCancelled(message:Message, description:any) {
+    const embed = new MessageEmbed()
+        .setDescription(description)
+        .setColor("#E74C3C")
+        .setFooter(`Requested By: ${message.author.tag} | Synic v1.0.0`);
+
+    await message.channel.send(embed);
+}
