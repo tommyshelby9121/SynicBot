8dd7614c24ebcf4d6855a4a1fa6352a0032d04de
Sycrixx <biometricmc@gmail.com>
2021-01-09 09:38:32 +1100

    Fixed guildCreate event

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -5,12 +5,14 @@ import { Guild } from "discord.js";
 import GuildInfo from "../models/GuildInfo";
 
 module.exports = async (client:SynicClient, guildData:Guild) => {
-    await GuildInfo.create({
+    const newGuild = await GuildInfo.create({
         guildId: guildData.id,
         guildName: guildData.name,
         memberCount: guildData.memberCount,
-        channels: guildData.channels,
-        roles: guildData.roles,
+        channels: guildData.channels.cache.array(),
+        roles: guildData.roles.cache.array(),
         region: guildData.region,
     });
+
+    await newGuild.save();
 }
