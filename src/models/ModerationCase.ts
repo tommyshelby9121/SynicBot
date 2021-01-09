bad28b61ebad61bb98fb0e6e62a8cb25eff03d70
Sycrixx <biometricmc@gmail.com>
2021-01-09 21:06:39 +1100

    Create ModerationCase schema

---
+++ src/models/ModerationCase.ts
@@ -0,0 +1,45 @@
+import { model, Schema, Document } from "mongoose";
+
+export interface IModCase extends Document {
+    guildId: string,
+    caseId: number,
+    staffMember: string,
+    victim: string,
+    reason: string,
+    method: string,
+    date: any,
+}
+
+const ModCaseSchema = new Schema({
+    guildId: {
+        type: String,
+        required: true,
+    },
+    caseId: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    staffMember: {
+        type: String,
+        required: true,
+    },
+    victim: {
+        type: String,
+        required: true,
+    },
+    reason: {
+        type: String,
+        required: true,
+    },
+    method: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+});
+
+export default model<IModCase>("ModerationCase", ModCaseSchema);
