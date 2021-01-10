72185114f4692f13925dd4856efb1b03632b80d0
Sycrixx <biometricmc@gmail.com>
2021-01-11 00:48:57 +1100

    Create modActionSuccess

--- src/utils/embedBuilder.ts
+++ src/utils/embedBuilder.ts
@@ -1,4 +1,5 @@
 import { Message, MessageEmbed } from "discord.js";
+import {emit} from "cluster";
 
 export async function noPermission(message:Message, description:any) {
     const embed = new MessageEmbed()
@@ -18,10 +19,19 @@ export async function noUser(message:Message, description:any) {
     await message.channel.send(embed);
 }
 
-export async function warnCancelled(message:Message, description:any) {
+export async function modActionCancelled(message:Message, description:any) {
     const embed = new MessageEmbed()
         .setDescription(description)
         .setColor("#E74C3C")
+        .setFooter(`Requested By: ${message.author.tag} | Synic v1.0.0`);
+
+    await message.channel.send(embed);
+}
+
+export async function modActionSuccess(message:Message, description:any) {
+    const embed = new MessageEmbed()
+        .setDescription(description)
+        .setColor("#58D68D")
         .setFooter(`Requested By: ${message.author.tag} | Synic v1.0.0`);
 
     await message.channel.send(embed);
