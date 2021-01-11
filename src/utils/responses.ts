f0ef29cb6ead9bc548c3fd7ed3f580064fd7a30b
Sycrixx <biometricmc@gmail.com>
2021-01-12 03:49:20 +1100

    Add kick & ban responses

--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -1,12 +1,20 @@
 export default {
     moderation: {
-      no_user_permission: "You do not have permission to execute this command!",
-      no_bot_permission: "I do not have permission to execute this command!",
-      no_user_provided: "Please provide a user to ",
-      warn: {
-        could_not_warn_user: "Could not warn user! Please remove all permissions from the user in order to warn them!",
-        cannot_warn_self: "You cannot warn yourself!"
-      },
-      no_case_arg: "Please provide a case number!",
+        no_user_permission: "You do not have permission to execute this command!",
+        no_bot_permission: "I do not have permission to execute this command!",
+        no_user_provided: "Please provide a user to ",
+        no_case_arg: "Please provide a case number!",
+        warn: {
+            could_not_warn_user: "Could not warn user! Please remove all permissions from the user in order to warn them!",
+            cannot_warn_self: "You cannot warn yourself!",
+        },
+        kick: {
+            could_not_kick_user: "Could not kick user! Please remove all permissions from the user in order to kick them!",
+            cannot_kick_self: "You cannot kick yourself!",
+        },
+        ban: {
+            could_not_ban_user: "Could not ban user! Please remove all permissions from the user in order to kick them!",
+            cannot_ban_self: "You cannot ban yourself!",
+        }
     }
 }
