6dfe97108e909a9d6f8f20022db4ab7fda80cd4e
Sycrixx <biometricmc@gmail.com>
2021-01-12 06:14:35 +1100

    Changed to responses & import

--- src/commands/moderation/warn.ts
+++ src/commands/moderation/warn.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 import { simpleEmbed } from "../../utils/embedBuilder";
 import colors from "../../utils/colors";
 import { formatDate } from "../../utils/formatDate";
-const responses = require("../../utils/responses");
+import responses from "../../utils/responses";
 
 // Import Model
 import GuildInfo from "../../models/GuildInfo";
@@ -14,12 +14,12 @@ export default {
     description: "Issue a warning to specified user",
     category: "moderation",
     async execute(client:SynicClient, message:Message, args:ReadonlyArray<string>) {
-        if (!message.member?.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.en.moderation.no_user_permission, colors.error);
-        if (!message.guild?.me?.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.en.moderation.no_bot_permission, colors.error);
+        if (!message.member?.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.moderation.no_user_permission, colors.error);
+        if (!message.guild?.me?.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.moderation.no_bot_permission, colors.error);
         const warnUser = message.guild?.member(message.mentions.users.first() || message.guild?.members.cache.get(args[0])!)
-        if (!warnUser) return simpleEmbed(message, responses.en.moderation.no_user_provided + "warn!", colors.error);
-        if (warnUser.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.en.moderation.warn.could_not_warn_user, colors.error);
-        if (warnUser.id === message.author.id) return simpleEmbed(message, responses.en.moderation.warn.cannot_warn_self, colors.error);
+        if (!warnUser) return simpleEmbed(message, responses.moderation.no_user_provided + "warn!", colors.error);
+        if (warnUser.hasPermission("MANAGE_MESSAGES")) return simpleEmbed(message, responses.moderation.warn.could_not_warn_user, colors.error);
+        if (warnUser.id === message.author.id) return simpleEmbed(message, responses.moderation.warn.cannot_warn_self, colors.error);
         let warnReason = args.join(" ").slice(warnUser.id.length);
         if (!warnReason) warnReason = "No Reason Provided";
 
